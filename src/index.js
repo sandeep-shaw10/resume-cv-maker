627efdfcdf36d8756c2e519e78a36da49e160677
Sandeep Shaw <sandeep10shaw@gmail.com>
2021-04-20 17:54:27 +0530

    Hash Router Upload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <HashRouter >
+        <App />
+    </HashRouter>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
