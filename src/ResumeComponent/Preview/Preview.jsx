5296de96ce824878a0e177a9a4d145067f94e40a
Sandeep Shaw <sandeep10shaw@gmail.com>
2021-04-25 21:15:24 +0530

    Preview Design Set!

--- src/ResumeComponent/Preview/Preview.jsx
+++ src/ResumeComponent/Preview/Preview.jsx
@@ -45,13 +45,53 @@ export default class Preview extends Component
                 <div className="content-preview container  m-1">
                         <div className="row">
                             <div className="col-3 ">
-                                <div className="wrap bg-info m-1">
-                                    <p>Quick Info </p>
+                                <div className="wrap-1 m-1">
+                                    <div className="social-media pb-4">
+                                        <h4>Social Media</h4>
+                                        <p> <i className="fa fa-github"></i> Github</p>
+                                        <p> <i className="fa fa-dribbble"></i> Dribbble</p>
+                                        <p> <i className="fa fa-facebook"></i> Facebook</p>
+                                        <p> <i className="fa fa-youtube"></i> Youtube</p>
+                                        <p> <i className="fa fa-twitter"></i> Twitter</p>
+                                    </div>
+
+                                    <div className="skills pb-4">
+                                        <h4>Technical Skills</h4>
+                                        <p> Github</p>
+                                        <p> Dribbble</p>
+                                        <p> Facebook</p>
+                                        <p> Youtube</p>
+                                        <p> Twitter</p>
+                                    </div>
+
+                                    <div className="language pb-4">
+                                        <h4>Languages</h4>
+                                        <p>Github</p>
+                                        <p>Dribbble</p>
+                                        <p>Facebook</p>
+                                        <p>Youtube</p>
+                                        <p>Twitter</p>
+                                    </div>
                                 </div>
                             </div>
                             <div className="col-9 ">
-                                <div className="wrap bg-info m-1">
-                                    <p>Quick Info </p>
+                                <div className="wrap-2 m-1">
+                                    <div className="work-experience pb-4">
+                                        <h2>Work Experience</h2>
+                                    </div>
+
+                                    <div className="project pb-4">
+                                        <h2>Projects</h2>
+                                    </div>
+
+                                    <div className="education pb-4">
+                                        <h2>Education</h2>
+                                    </div>
+
+                                    <div className="awards pb-4">
+                                        <h2>Achievements</h2>
+                                    </div>
+                                    
                                 </div>
                             </div>
                         </div>
