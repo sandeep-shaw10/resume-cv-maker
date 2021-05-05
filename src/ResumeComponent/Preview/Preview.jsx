be767312977c581fcaceb0b30e5cd95c78be9d00
Sandeep Shaw <sandeep10shaw@gmail.com>
2021-05-05 23:29:18 +0530

    Preview

--- src/ResumeComponent/Preview/Preview.jsx
+++ src/ResumeComponent/Preview/Preview.jsx
@@ -4,12 +4,30 @@ import './Preview.css'
 
 export default class Preview extends Component 
 {
+    constructor(){
+        super();
+        this.yearFormatting = this.yearFormatting.bind(this);
+    }
+
+    yearFormatting = (from,to) => {
+        let format = "";
+        format = (to != "")? "( "+from+" - "+to+" )":"( "+from+" )";
+        return format;
+    }
+
     render() 
     {
         //console.log(this.props);
         var prox = this.props.data;
         var image = prox.personal["image"];
         var imageClass = (image === '')?"col-12 personal-detail":"col-9 personal-detail";
+        var education = prox.education;
+        var work = prox.work;
+        var awards = prox.awards;
+        var project = prox.project;
+        var skill = prox.skill;
+        var language = prox.language;
+        var social = prox.social;
 
         return (
             <>
@@ -35,10 +53,10 @@ export default class Preview extends Component
 
                 <div className="media-preview container">
                     <div className="row p-1 ">
-                        <div className="col-3 d-flex "><i className="fa fa-mobile"></i> <p className="ml-1"> {prox.personal["phone"]} </p></div>
-                        <div className="col-3 d-flex "><i className="fa fa-envelope"></i> <p className="ml-1"> {prox.personal["email"]} </p></div>
-                        <div className="col-3 d-flex "><i className="fa fa-globe"></i> <p className="ml-1"> {prox.personal["website"]} </p> </div>
-                        <div className="col-3 d-flex "><i className="fa fa-linkedin"></i> <p className="ml-1"> {prox.social["LinkedIn"]} </p> </div>
+                        {(prox.personal["phone"] != "")?<div className="col-3 d-flex "><i className="fa fa-mobile"></i> <p className="ml-1"> {prox.personal["phone"]} </p></div>:""}
+                        {(prox.personal["email"] != "")?<div className="col-3 d-flex "><i className="fa fa-envelope"></i> <p className="ml-1"> {prox.personal["email"]} </p></div>:""}
+                        {(prox.personal["website"] != "")?<div className="col-3 d-flex "><i className="fa fa-globe"></i> <p className="ml-1"> {prox.personal["website"]} </p> </div>:""}
+                        {(social.LinkedIn != "")?<div className="col-3 d-flex "><i className="fa fa-linkedin"></i> <p className="ml-1"> {social.LinkedIn} </p> </div>:""}
                     </div>
                 </div>
 
@@ -46,51 +64,114 @@ export default class Preview extends Component
                         <div className="row">
                             <div className="col-3 ">
                                 <div className="wrap-1 m-1">
-                                    <div className="social-media pb-4">
-                                        <h4>Social Media</h4>
-                                        <p> <i className="fa fa-github"></i> Github</p>
-                                        <p> <i className="fa fa-dribbble"></i> Dribbble</p>
-                                        <p> <i className="fa fa-facebook"></i> Facebook</p>
-                                        <p> <i className="fa fa-youtube"></i> Youtube</p>
-                                        <p> <i className="fa fa-twitter"></i> Twitter</p>
-                                    </div>
+                                    {/* Objects -> Value in list -> Filter the empty value -> length */}
+                                    {
+                                        (Object.values(social).filter(x => x != "").length != 0)?
+                                        <div className="social-media pb-4">
+                                            <h4>Social Media</h4>
+                                            {(social.Github != ""? <p> <i className="fa fa-github"></i> {social.Github} </p> :"")}
+                                            {(social.Dribbble != ""? <p> <i className="fa fa-dribbble"></i> {social.Dribbble} </p> :"")}
+                                            {(social.Facebook != ""? <p> <i className="fa fa-facebook"></i> {social.Facebook} </p> :"")}
+                                            {(social.Youtube != ""? <p> <i className="fa fa-youtube"></i> {social.Youtube} </p> :"")}
+                                            {(social.Twitter != ""? <p> <i className="fa fa-twitter"></i> {social.Twitter} </p> :"")}
+                                        </div>
+                                        :""
+                                    }
 
-                                    <div className="skills pb-4">
-                                        <h4>Technical Skills</h4>
-                                        <p> Github</p>
-                                        <p> Dribbble</p>
-                                        <p> Facebook</p>
-                                        <p> Youtube</p>
-                                        <p> Twitter</p>
-                                    </div>
+                                    {
+                                        (skill.length != 0)?
+                                        <div className="skills pb-4">
+                                            <h4>Technical Skills</h4>
+                                            {skill.map((x) => <p>{x}</p> )}
+                                        </div>
+                                        :""
+                                    }
 
-                                    <div className="language pb-4">
-                                        <h4>Languages</h4>
-                                        <p>Github</p>
-                                        <p>Dribbble</p>
-                                        <p>Facebook</p>
-                                        <p>Youtube</p>
-                                        <p>Twitter</p>
-                                    </div>
+                                    {
+                                        (language.length != 0)?
+                                        <div className="language pb-4">
+                                            <h4>Languages</h4>
+                                            {language.map((x) => <p>{x}</p> )}
+                                        </div>
+                                        :""
+                                    }                                   
+
                                 </div>
                             </div>
                             <div className="col-9 ">
                                 <div className="wrap-2 m-1">
-                                    <div className="work-experience pb-4">
-                                        <h2>Work Experience</h2>
-                                    </div>
+                                    
+                                        {
+                                            (Object.values(work).length != 0)?
+                                            <div className="work-experience pb-4">
+                                                <h2>Work Experience</h2>
+                                                {(Object.values(work).map(x => 
+                                                        <div className="wrap mb-2" key={x.work}>
+                                                            <div className="d-flex justify-content-between">
+                                                                <p className="m-0 lead"><cite>{x.work}</cite></p>
+                                                                <p className="text-right m-0 lead"> {this.yearFormatting(x.from,x.to)} </p>
+                                                            </div>
+                                                            <p className="text-muted m-0">{x.name }</p> 
+                                                            <div className="wrap-about-resume text-muted m-0" dangerouslySetInnerHTML={{__html: x.about}}></div>
+                                                        </div> 
+                                                ))}
+                                            </div>
+                                            :""
+                                        }
+                                    
+                                        {
+                                            (Object.values(project).length != 0)?
+                                            <div className="projec pb-4">
+                                                <h2>Projects</h2>
+                                                {(Object.values(project).map(x => 
+                                                        <div className="wrap mb-2" key={x.work}>
+                                                            <div className="d-flex justify-content-between">
+                                                                <p className="m-0 lead"><cite>{x.work}</cite></p>
+                                                                <p className="text-right m-0 lead"> {this.yearFormatting(x.from,x.to)} </p>
+                                                            </div>
+                                                            <p className="text-muted m-0">{x.name }</p> 
+                                                            <div className="wrap-about-resume text-muted m-0" dangerouslySetInnerHTML={{__html: x.about}}></div>
+                                                        </div> 
+                                                ))}
+                                            </div>
+                                            :""
+                                        }
 
-                                    <div className="project pb-4">
-                                        <h2>Projects</h2>
-                                    </div>
+                                        {
+                                            (Object.values(education).length != 0)?
+                                            <div className="education pb-4">
+                                                <h2>Education</h2>
+                                                {(Object.values(education).map(x => 
+                                                        <div className="wrap mb-2" key={x.degree}>
+                                                            <div className="d-flex justify-content-between">
+                                                                <p className="m-0 lead"><cite>{x.degree}</cite></p>
+                                                                <p className="text-right m-0 lead"> {this.yearFormatting(x.from,x.to)} </p>
+                                                            </div>
+                                                            <p className="text-muted m-0">{x.name }</p> 
+                                                            <div className="wrap-about-resume text-muted m-0" dangerouslySetInnerHTML={{__html: x.about}}></div>
+                                                        </div> 
+                                                ))}           
+                                            </div>
+                                            :""
+                                        } 
 
-                                    <div className="education pb-4">
-                                        <h2>Education</h2>
-                                    </div>
-
-                                    <div className="awards pb-4">
-                                        <h2>Achievements</h2>
-                                    </div>
+                                        {
+                                            (Object.values(awards).length != 0)?
+                                            <div className="awards pb-4">
+                                                <h2>Achievements</h2>
+                                                {(Object.values(awards).map(x => 
+                                                        <div className="wrap mb-2" key={x.work}>
+                                                            <div className="d-flex justify-content-between">
+                                                                <p className="m-0 lead"><cite>{x.work}</cite></p>
+                                                                <p className="text-right m-0 lead"> {this.yearFormatting(x.from,x.to)} </p>
+                                                            </div>
+                                                            <p className="text-muted m-0">{x.name }</p> 
+                                                            <div className="wrap-about-resume text-muted m-0" dangerouslySetInnerHTML={{__html: x.about}}></div>
+                                                        </div> 
+                                                ))}
+                                            </div>
+                                            :""
+                                        }
                                     
                                 </div>
                             </div>
