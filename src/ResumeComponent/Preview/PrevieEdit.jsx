12ae119dd9a3eaed5f2b1748001f640281e88ffe
Sandeep Shaw <sandeep10shaw@gmail.com>
2021-05-11 23:43:36 +0530

    Preview Customization

---
+++ src/ResumeComponent/Preview/PrevieEdit.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+
+export default class PrevieEdit extends Component 
+{
+    constructor(props){
+        super(props);
+        this.removeImage = this.removeImage.bind(this);
+        this.printResume = this.printResume.bind(this);
+        this.themeColor = this.themeColor.bind(this);
+    }
+
+    removeImage = () => {
+        let x = this.props.data
+        x.data.personal["image"] = ''
+        x.updateState(x.social)
+    }
+
+    printResume = () => {
+        window.print();
+    }
+
+    themeColor = (e) => {
+        let color = e.target.value;
+        this.props.themeColor.theme = color;
+        let x = this.props;
+        this.props.updateTheme(x.theme);
+    }
+
+    textColor = (e) => {
+        let color = e.target.value;
+        this.props.themeColor.color = color;
+        let x = this.props;
+        this.props.updateTheme(x.color);
+    }
+
+    render() {
+
+        return (
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className ="btn-group btn-group-sm">
+                            <a href="/resume/details" type="button" className="btn btn-primary">Insert Image</a>
+                            <button type="button" className="btn btn-danger" onClick={this.removeImage}>Remove Image</button>
+                            <button type="button" className="btn btn-warning" onClick={this.printResume}>Print Resume</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="container my-3">
+                    <div className="row">
+                        <div className="col-6">
+                            <label htmlFor="favcolor1" className="label mr-4">Theme Colour: </label>
+                            <input type="color" id="favcolor1" name="favcolor1" defaultValue={this.props.themeColor.theme} onChange={(e) => {this.themeColor(e)}} />
+                        </div>
+                        <div className="col-6">
+                            <label htmlFor="favcolor1" className="label mr-4">Text Colour: </label>
+                            <input type="color" id="favcolor1" name="favcolor1" defaultValue={this.props.themeColor.color} onChange={(e) => {this.textColor(e)}} />
+                        </div>
+                     </div>
+                </div>
+            </div>
+        )
+    }
+}
--- src/ResumeComponent/Preview/Preview.jsx
+++ src/ResumeComponent/Preview/Preview.jsx
@@ -1,18 +1,28 @@
 import React, { Component } from 'react'
 import Resume from '../../PageComponents/Resume'
+import PrevieEdit from './PrevieEdit';
 import './Preview.css'
 
 export default class Preview extends Component 
 {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.yearFormatting = this.yearFormatting.bind(this);
+        this.state = {
+            theme: '#ffb9b9',
+            color: '#262626',
+        }
+        this.updateTheme = this.updateTheme.bind(this);
     }
 
     yearFormatting = (from,to) => {
         let format = "";
-        format = (to != "")? "( "+from+" - "+to+" )":"( "+from+" )";
+        format = (to !== "")? "( "+from+" - "+to+" )":"( "+from+" )";
         return format;
+    }
+
+    updateTheme = (x) => {
+        this.setState({...x});
     }
 
     render() 
@@ -20,7 +30,7 @@ export default class Preview extends Component
         //console.log(this.props);
         var prox = this.props.data;
         var image = prox.personal["image"];
-        var imageClass = (image === '')?"col-12 personal-detail":"col-9 personal-detail";
+        var imageClass = (image === '')?"col-12":"col-9";
         var education = prox.education;
         var work = prox.work;
         var awards = prox.awards;
@@ -28,12 +38,28 @@ export default class Preview extends Component
         var skill = prox.skill;
         var language = prox.language;
         var social = prox.social;
+                
+        //Styling
+        const personalDetail = {
+            backgroundColor: this.state.theme,
+        }
 
-        return (
+        const borderBottom = {
+            borderBottom: '2px solid '+this.state.theme,
+        }
+
+        const textColor = {
+            color: this.state.color,
+        }
+
+        return ( 
             <>
             <Resume updateState={this.props.updateState} />
 
-            <div className="container my-4 bg-light py-3" id="resume">
+            <PrevieEdit data={this.props} updateTheme={this.updateTheme} themeColor={this.state}/>
+
+
+            <div className="container my-4 bg-light py-3" id="resume" style={textColor}>
 
                 <div className="detail-preview container">
                     <div className="row">
@@ -42,7 +68,7 @@ export default class Preview extends Component
                             <img className="img-fluid" src={image} alt="Re-upload if not displayed" />
                         </div>
                         }
-                        <div className={imageClass}>
+                        <div className={imageClass} style={personalDetail}>
                             <h1>{prox.personal["name"]}</h1>
                             <h5>{prox.personal["occupation"]}</h5>
                             <hr className="m-1"/>
@@ -53,45 +79,49 @@ export default class Preview extends Component
 
                 <div className="media-preview container">
                     <div className="row p-1 ">
-                        {(prox.personal["phone"] != "")?<div className="col-3 d-flex "><i className="fa fa-mobile"></i> <p className="ml-1"> {prox.personal["phone"]} </p></div>:""}
-                        {(prox.personal["email"] != "")?<div className="col-3 d-flex "><i className="fa fa-envelope"></i> <p className="ml-1"> {prox.personal["email"]} </p></div>:""}
-                        {(prox.personal["website"] != "")?<div className="col-3 d-flex "><i className="fa fa-globe"></i> <p className="ml-1"> {prox.personal["website"]} </p> </div>:""}
-                        {(social.LinkedIn != "")?<div className="col-3 d-flex "><i className="fa fa-linkedin"></i> <p className="ml-1"> {social.LinkedIn} </p> </div>:""}
+                        {(prox.personal["phone"] !== "")?<div className="col-3 d-flex "><i className="fa fa-mobile"></i> <p className="ml-1"> {prox.personal["phone"]} </p></div>:""}
+                        {(prox.personal["email"] !== "")?<div className="col-3 d-flex "><i className="fa fa-envelope"></i> <p className="ml-1"> {prox.personal["email"]} </p></div>:""}
+                        {(prox.personal["website"] !== "")?<div className="col-3 d-flex "><i className="fa fa-globe"></i> <p className="ml-1"> {prox.personal["website"]} </p> </div>:""}
+                        {(social.LinkedIn !== "")?<div className="col-3 d-flex "><i className="fa fa-linkedin"></i> <p className="ml-1"> {social.LinkedIn} </p> </div>:""}
                     </div>
                 </div>
 
                 <div className="content-preview container  m-1">
                         <div className="row">
                             <div className="col-3 ">
-                                <div className="wrap-1 m-1">
+                                <div className="wrap-1 m-1" style = {{borderRight: '2px solid '+this.state.theme}}>
                                     {/* Objects -> Value in list -> Filter the empty value -> length */}
                                     {
-                                        (Object.values(social).filter(x => x != "").length != 0)?
+                                        (Object.values(social).filter(x => x !== "").length !== 0)?
                                         <div className="social-media pb-4">
                                             <h4>Social Media</h4>
-                                            {(social.Github != ""? <p> <i className="fa fa-github"></i> {social.Github} </p> :"")}
-                                            {(social.Dribbble != ""? <p> <i className="fa fa-dribbble"></i> {social.Dribbble} </p> :"")}
-                                            {(social.Facebook != ""? <p> <i className="fa fa-facebook"></i> {social.Facebook} </p> :"")}
-                                            {(social.Youtube != ""? <p> <i className="fa fa-youtube"></i> {social.Youtube} </p> :"")}
-                                            {(social.Twitter != ""? <p> <i className="fa fa-twitter"></i> {social.Twitter} </p> :"")}
+                                            {(social.Github !== ""? <p> <i className="fa fa-github"></i> {social.Github} </p> :"")}
+                                            {(social.Dribbble !== ""? <p> <i className="fa fa-dribbble"></i> {social.Dribbble} </p> :"")}
+                                            {(social.Facebook !== ""? <p> <i className="fa fa-facebook"></i> {social.Facebook} </p> :"")}
+                                            {(social.Youtube !== ""? <p> <i className="fa fa-youtube"></i> {social.Youtube} </p> :"")}
+                                            {(social.Twitter !== ""? <p> <i className="fa fa-twitter"></i> {social.Twitter} </p> :"")}
                                         </div>
                                         :""
                                     }
 
                                     {
-                                        (skill.length != 0)?
-                                        <div className="skills pb-4">
+                                        (skill.length !== 0)?
+                                        <div className="skills pb-4 ">
                                             <h4>Technical Skills</h4>
-                                            {skill.map((x) => <p>{x}</p> )}
+                                            <ul className="ml-2">
+                                                {skill.map((x) => <li key={x} className="mb-0">{x}</li> )}
+                                            </ul>
                                         </div>
                                         :""
                                     }
 
                                     {
-                                        (language.length != 0)?
+                                        (language.length !== 0)?
                                         <div className="language pb-4">
                                             <h4>Languages</h4>
-                                            {language.map((x) => <p>{x}</p> )}
+                                            <ul className="ml-2">
+                                                {language.map((x) => <li key={x} className="mb-0">{x}</li> )}
+                                            </ul>
                                         </div>
                                         :""
                                     }                                   
@@ -102,9 +132,9 @@ export default class Preview extends Component
                                 <div className="wrap-2 m-1">
                                     
                                         {
-                                            (Object.values(work).length != 0)?
+                                            (Object.values(work).length !== 0)?
                                             <div className="work-experience pb-4">
-                                                <h2>Work Experience</h2>
+                                                <h2 style={borderBottom}>Work Experience</h2>
                                                 {(Object.values(work).map(x => 
                                                         <div className="wrap mb-2" key={x.work}>
                                                             <div className="d-flex justify-content-between">
@@ -120,9 +150,9 @@ export default class Preview extends Component
                                         }
                                     
                                         {
-                                            (Object.values(project).length != 0)?
+                                            (Object.values(project).length !== 0)?
                                             <div className="projec pb-4">
-                                                <h2>Projects</h2>
+                                                <h2 style={borderBottom}>Projects</h2>
                                                 {(Object.values(project).map(x => 
                                                         <div className="wrap mb-2" key={x.work}>
                                                             <div className="d-flex justify-content-between">
@@ -138,9 +168,9 @@ export default class Preview extends Component
                                         }
 
                                         {
-                                            (Object.values(education).length != 0)?
+                                            (Object.values(education).length !== 0)?
                                             <div className="education pb-4">
-                                                <h2>Education</h2>
+                                                <h2 style={borderBottom}>Education</h2>
                                                 {(Object.values(education).map(x => 
                                                         <div className="wrap mb-2" key={x.degree}>
                                                             <div className="d-flex justify-content-between">
@@ -156,9 +186,9 @@ export default class Preview extends Component
                                         } 
 
                                         {
-                                            (Object.values(awards).length != 0)?
+                                            (Object.values(awards).length !== 0)?
                                             <div className="awards pb-4">
-                                                <h2>Achievements</h2>
+                                                <h2 style={borderBottom}>Achievements</h2>
                                                 {(Object.values(awards).map(x => 
                                                         <div className="wrap mb-2" key={x.work}>
                                                             <div className="d-flex justify-content-between">
@@ -177,8 +207,7 @@ export default class Preview extends Component
                             </div>
                         </div>
                 </div>
-
-            </div>
+            </div>          
             </>
         )
     }
